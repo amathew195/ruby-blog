2e3ea6430e017785e901dd3a7faed3d0872b0c3e
Ashley Mathew <amathew195@gmail.com>
2023-02-11 15:39:08 -0600

    add users table and user model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum: 6, maximum: 100}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
+
+    belongs_to :user
+
+    def get_title
+        "The title of this article is #{self.title}"
+    end
+
 end
